17402822f094cb8669d0c764aabcb12d14f077a3
abhay-bathani <abhaybathani3024@gmail.com>
2025-12-12 11:33:05 +0530

    Save local changes

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+const connectDB = require("./config/db");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
+
+connectDB();
+
+// Routes
+const userRoutes = require("./routes/User");
+app.use("/api/users", userRoutes);
+
+const adminRoutes = require("./routes/Admin");
+app.use("/api/admin", adminRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
